ca79595144f22288811af387f40a4b14a55c88bb
PaulDok <pauldok@rambler.ru>
2017-01-29 10:54:04 +0300

    Правки по комментариям

--- js/form.js
+++ js/form.js
@@ -1,9 +1,20 @@
 'use strict';
 
-// Preparation
+// Variables
 var editOverlay = document.getElementsByClassName('upload-overlay')[0];
 var uploadOverlay = document.getElementById('upload-select-image');
+var uploadSelectInput = document.getElementById('upload-file');
+var uploadFormCancelButton = editOverlay.getElementsByClassName('upload-form-cancel')[0];
+var filterOptionButtons = document.getElementsByName('upload-filter');
+var image = editOverlay.getElementsByClassName('filter-image-preview')[0];
+var imageBaseClass = image.className;
+var resizeDecreaseButton = editOverlay.getElementsByClassName('upload-resize-controls-button-dec')[0];
+var resizeIncreaseButton = editOverlay.getElementsByClassName('upload-resize-controls-button-inc')[0];
+var resizeValueField = editOverlay.getElementsByClassName('upload-resize-controls-value')[0];
+var currentSize = resizeValueField.value;
+var resizeStep = 25;
 
+// Preparation
 var showUploadAndHideEdit = function () {
   editOverlay.classList.add('invisible');
   uploadOverlay.classList.remove('invisible');
@@ -17,49 +28,35 @@ var showEditAndHideUpload = function () {
 showUploadAndHideEdit();
 
 // Show / hide edit form
-var uploadSelectInput = document.getElementById('upload-file');
-uploadSelectInput.onchange = showEditAndHideUpload();
-
-var uploadFormCancelButton = document.getElementsByClassName('upload-form-cancel')[0];
+uploadSelectInput.addEventListener('click', showEditAndHideUpload);
 uploadFormCancelButton.addEventListener('click', showUploadAndHideEdit);
 
 // Filters application
-var filterOptionButtons = document.getElementsByName('upload-filter');
-var image = document.getElementsByClassName('filter-image-preview')[0];
-var imageBaseClass = image.className;
-
-var addFilterToImage = function (filterOption) {
-  image.className = imageBaseClass + ' filter-' + filterOption;
+var addFilterToImage = function () {
+  image.className = imageBaseClass + ' filter-' + this.value;
 };
 
 for (var i = 0; i < filterOptionButtons.length; i++) {
   var filterOptionButton = filterOptionButtons[i];
-  filterOptionButton.addEventListener('click', addFilterToImage.bind(null, filterOptionButton.value));
+  filterOptionButton.addEventListener('click', addFilterToImage);
 }
 
 // Image scaling: buttons
-var resizeDecreaseButton = document.getElementsByClassName('upload-resize-controls-button-dec')[0];
-var resizeIncreaseButton = document.getElementsByClassName('upload-resize-controls-button-inc')[0];
-var resizeValueField = document.getElementsByClassName('upload-resize-controls-value')[0];
-
-var resizeStep = 25;
-
-var resizeImage = function (delta) {
-  var oldSizeValue = resizeValueField.value;
-  oldSizeValue = parseInt(oldSizeValue.slice(0, -1), 10);
-  var newSizeValue = Math.min(100, oldSizeValue + delta);
-  newSizeValue = Math.max(25, newSizeValue);
-  changeScaling(newSizeValue);
-  newSizeValue += '%';
-  resizeValueField.value = newSizeValue;
+var resizeImage = function (value) {
+  resizeValueField.value = value + '%';
+  changeScaling(value);
 };
 
 var resizeImageDown = function () {
-  resizeImage(-resizeStep);
+  currentSize -= resizeStep;
+  currentSize = Math.max(25, currentSize);
+  resizeImage(currentSize);
 };
 
 var resizeImageUp = function () {
-  resizeImage(resizeStep);
+  currentSize += resizeStep;
+  currentSize = Math.min(100, currentSize);
+  resizeImage(currentSize);
 };
 
 resizeDecreaseButton.addEventListener('click', resizeImageDown);
@@ -73,4 +70,5 @@ var changeScaling = function (scalePercent) {
 
 // Initialize: Reset sizing field value to 100%
 resizeValueField.value = '100%';
+currentSize = 100;
 changeScaling(100);
